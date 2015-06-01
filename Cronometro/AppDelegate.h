d631d580a2dab14b526f84b843cca5ea5d517d9f
SandraGuzman <sandra.guzba18@gmail.com>
2015-06-01 12:20:57 -0500

    Initial Commit

---
+++ Cronometro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cronometro
+//
+//  Created by Sandra Guzmán Bautista on 01/06/15.
+//  Copyright (c) 2015 onikom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cronometro/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Cronometro
+//
+//  Created by Sandra Guzmán Bautista on 01/06/15.
+//  Copyright (c) 2015 onikom. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
